04e3579f0b0961b3d18f3d7c5948340a86c76784
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-04-13 17:49:22 +0100

    Adding second type of crosshair

--- js/@script.js
+++ js/@script.js
@@ -250,22 +250,59 @@ function drawHealthBar() {
         ctx.strokeRect(canvas.width - 110, canvas.height - 35, 100, 10);
     }
 }
+var crossHairType = {
+    "standard": 0,
+    "sniper": 1
+};
+var crossHairs = [
+    {
+        name: "standard",
+        draw: function () {
+            ctx.strokeStyle = "rgba(0,0,255,0.8)";
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2 - 10, canvas.height / 2);
+            ctx.lineTo(canvas.width / 2 - 20, canvas.height / 2);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2 + 10, canvas.height / 2);
+            ctx.lineTo(canvas.width / 2 + 20, canvas.height / 2);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2, canvas.height / 2 - 10);
+            ctx.lineTo(canvas.width / 2, canvas.height / 2 - 20);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2, canvas.height / 2 + 10);
+            ctx.lineTo(canvas.width / 2, canvas.height / 2 + 20);
+            ctx.stroke();
+            ctx.strokeStyle = "black";
+        }
+    },
+    {
+        name: "sniper",
+        draw: function () {
+            ctx.strokeStyle = "rgba(0,0,255,0.8)";
+            ctx.beginPath();
+            ctx.arc(canvas.width / 2, canvas.height / 2, 20, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.arc(canvas.width / 2, canvas.height / 2, 40, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2 - 50, canvas.height / 2);
+            ctx.lineTo(canvas.width / 2 + 50, canvas.height / 2);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2, canvas.height / 2 - 50);
+            ctx.lineTo(canvas.width / 2, canvas.height / 2 + 50);
+            ctx.stroke();
+            ctx.strokeStyle = "black";
+        }
+    }
+];
+var currentCrossHair = crossHairType.standard;
 function drawCrossHair() {
-    ctx.strokeStyle = "rgba(0,0,255,0.8)";
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, 20, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, 40, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2 - 50, canvas.height / 2);
-    ctx.lineTo(canvas.width / 2 + 50, canvas.height / 2);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2, canvas.height / 2 - 50);
-    ctx.lineTo(canvas.width / 2, canvas.height / 2 + 50);
-    ctx.stroke();
+    crossHairs[currentCrossHair].draw();
 }
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
--- js/displays/crosshair.ts
+++ js/displays/crosshair.ts
@@ -1,17 +1,55 @@
-﻿function drawCrossHair() {
-    ctx.strokeStyle = "rgba(0,0,255,0.8)";
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, 20, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, 40, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2 - 50, canvas.height / 2);
-    ctx.lineTo(canvas.width / 2 + 50, canvas.height / 2);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2, canvas.height / 2 - 50);
-    ctx.lineTo(canvas.width / 2, canvas.height / 2 + 50);
-    ctx.stroke();
+﻿var crossHairType = {
+    "standard":0,
+    "sniper": 1
+}
+
+var crossHairs = [
+    {
+        name: "standard", draw: function () {
+            ctx.strokeStyle = "rgba(0,0,255,0.8)";
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2 - 10, canvas.height / 2);
+            ctx.lineTo(canvas.width / 2 - 20, canvas.height / 2);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2 + 10, canvas.height / 2);
+            ctx.lineTo(canvas.width / 2 + 20, canvas.height / 2);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2, canvas.height / 2 - 10);
+            ctx.lineTo(canvas.width / 2, canvas.height / 2 - 20);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2, canvas.height / 2 + 10);
+            ctx.lineTo(canvas.width / 2, canvas.height / 2 + 20);
+            ctx.stroke();
+            ctx.strokeStyle = "black";
+        }
+    },
+    {
+        name: "sniper", draw: function () {
+            ctx.strokeStyle = "rgba(0,0,255,0.8)";
+            ctx.beginPath();
+            ctx.arc(canvas.width / 2, canvas.height / 2, 20, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.arc(canvas.width / 2, canvas.height / 2, 40, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2 - 50, canvas.height / 2);
+            ctx.lineTo(canvas.width / 2 + 50, canvas.height / 2);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(canvas.width / 2, canvas.height / 2 - 50);
+            ctx.lineTo(canvas.width / 2, canvas.height / 2 + 50);
+            ctx.stroke();
+            ctx.strokeStyle = "black";
+        }
+    }
+]
+
+var currentCrossHair = crossHairType.standard;
+
+function drawCrossHair() {
+    crossHairs[currentCrossHair].draw();
 }
